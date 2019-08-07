fcd3d697e359e7401c9cc3d77882954b2ab99f5f
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-07 20:16:38 +0900

    adding my current handson machine learning algorithms

--- 00_HANDSON_ML_SCIKITLEARN_KERAS_TENSORFLOW/PARTI_TheFundamentals_of_Machine_Learning/02_End_To_End_Machine_Learning_Project/P02_End_To_End_Machine_Learning_Project.py
+++ 00_HANDSON_ML_SCIKITLEARN_KERAS_TENSORFLOW/PARTI_TheFundamentals_of_Machine_Learning/02_End_To_End_Machine_Learning_Project/P02_End_To_End_Machine_Learning_Project.py
@@ -37,6 +37,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from zlib import crc32   # This library to create a hash identifier to not repeat our training set
+from sklearn.model_selection import train_test_split
+
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -71,6 +74,43 @@ def split_train_test(data,test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+def ensure_our_dataset_no_repetition(data):
+    print("==============================================================================")
+    print("======= Adding a Hashing Data to prevent from using same testing set =========")
+    print("==============================================================================")
+    housing_with_id = data.reset_index() # adds an `index` column
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+    # This is the most stable method with identifer column since we don't have before
+    housing_with_id["id"] = data["longitude"] * 1000 + data["latitude"]
+    train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+    print("Original Data set")
+    print(housing_with_id)
+    print("Training set")
+    print(train_set)
+    print("Testing set")
+    print(test_set)
+
+
+def spliting_another_method_from_sklearn(data):
+    print("==============================================================================")
+    print("================ SPLITING USING SKLEARN LIBRARY ==============================")
+    print("==============================================================================")
+    # Here you will need sklearn.model_selection
+    train_set, test_set = train_test_split(data, test_size = 0.2, random_state = 42)
+    print("Original Data set")
+    print(data)
+    print("Training set")
+    print(train_set)
+    print("Testing set")
+    print(test_set)
 
 if __name__ == "__main__":
     fetch_housing_data()
@@ -81,5 +121,5 @@ if __name__ == "__main__":
     #Housing_hist()
     train_set, test_set = split_train_test(housing, 0.2)
     print(f"The size of training set is = {len(train_set)} and the size of test set is = {len(test_set)}")
-
-
+    #ensure_our_dataset_no_repetition(housing)
+    spliting_another_method_from_sklearn(housing)
