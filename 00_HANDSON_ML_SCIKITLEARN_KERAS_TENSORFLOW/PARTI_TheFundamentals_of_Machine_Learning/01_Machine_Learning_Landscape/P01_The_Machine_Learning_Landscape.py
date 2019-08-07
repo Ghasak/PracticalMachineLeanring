806241572513afffd7defcdd47afa7e15de18fb2
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-07 18:56:33 +0900

    updating my repo

---
+++ 00_HANDSON_ML_SCIKITLEARN_KERAS_TENSORFLOW/PARTI_TheFundamentals_of_Machine_Learning/01_Machine_Learning_Landscape/P01_The_Machine_Learning_Landscape.py
@@ -0,0 +1,22 @@
+'''
+    Part I. The fundamentals of machine learning
+        1- The Machine learning landscape
+        What Is Machine Learning?
+        Why Use Machine Learning?
+        Types of Machine Learning Systems 8
+        Supervised/Unsupervised Learning 8 Batch and Online Learning
+        Instance-Based Versus Model-Based Learning 18
+        Main Challenges of Machine Learning
+        Insufficient Quantity of Training Data
+        Nonrepresentative Training Data
+        Poor-Quality Data
+        Irrelevant Features
+        Overfitting the Training Data
+        Underfitting the Training Data
+        Stepping Back
+        Testing and Validating 31
+        Hyperparameter Tuning and Model Selection 32
+        Data Mismatch 33
+        Exercises 34
+'''
+
