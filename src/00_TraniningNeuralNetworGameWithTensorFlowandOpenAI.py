984fad45806eb939eca9d08ceb3fe586031eb680
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-07 17:36:08 +0900

    updating my repo

---
+++ src/00_TraniningNeuralNetworGameWithTensorFlowandOpenAI.py
@@ -0,0 +1,10 @@
+'''
+    - Intro Traning a neural network to play a game with TensorFlow and Open AI
+'''
+# ====================================================================
+import os
+CURRENT_DIR = os.getcwd()
+# Where our positive files
+## POSITIVE_DIR = os.path.join(CURRENT_DIR,'resources/positiveData.txt')
+
+print(CURRENT_DIR)
