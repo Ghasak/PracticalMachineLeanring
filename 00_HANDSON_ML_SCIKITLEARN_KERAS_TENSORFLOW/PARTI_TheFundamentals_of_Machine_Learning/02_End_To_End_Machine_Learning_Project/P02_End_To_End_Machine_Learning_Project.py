aa4c27e185e1af6f3b8bde200cb4247953ad6931
Ghasak Ibrahim <mdotgh@gmail.com>
2019-08-07 19:05:09 +0900

    adding my current handson machine learning algorithms

---
+++ 00_HANDSON_ML_SCIKITLEARN_KERAS_TENSORFLOW/PARTI_TheFundamentals_of_Machine_Learning/02_End_To_End_Machine_Learning_Project/P02_End_To_End_Machine_Learning_Project.py
@@ -0,0 +1,23 @@
+'''
+    CHAPTER TWO
+        End-to-End Machine Learning Project
+        1. Look at the big picture.
+        2. Get the data.
+        3. Discover and visualize the data to gain insights.
+        4. Prepare the data for Machine Learning algorithms.
+        5. Select a model and train it.
+        6. Fine-tune your model.
+        7. Present your solution.
+        8. Launch, monitor, and maintain your system.
+
+    - Using ENV00_HANDSON_ML_SCIKITLEARN_KERAS_TENSORFLOW -Environment Python 3.7.x MacPro
+'''
+
+# We will use we chose the California Housing Prices dataset from the StatLib repository
+
+
+'''
+        Welcome to Machine Learning Housing Corporation! The first task you are asked to perform is to build a model of housing prices in California using the California cen‐ sus data. This data has metrics such as the population, median income, median housing price, and so on for each block group in California. Block groups are the smallest geographical unit for which the US Census Bureau publishes sample data (a block group typically has a population of 600 to 3,000 people). We will just call them “districts” for short.
+        Your model should learn from this data and be able to predict the median housing price in any district, given all the other metrics.
+'''
+
